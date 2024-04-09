179037ca705787f8712a8aa3d34d0ff330a4deae
Nodja <Nodja@users.noreply.github.com>
2024-04-08 22:25:47 -0400

    add simple welcome page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,24 +2,11 @@
 	import { Button } from 'svelte-ux';
 </script>
 
-<main class="p-2 grid place-items-center content-center">
-	<h1 class="text-2xl font-semibold mb-2">Welcome to ✨Svelte UX✨</h1>
-	<div>
-		<Button
-			href="https://svelte-ux.techniq.dev"
-			target="_blank"
-			variant="fill-light"
-			color="primary"
-		>
-			Documentation
-		</Button>
-		<Button
-			href="https://svelte-ux.techniq.dev/customization"
-			target="_blank"
-			variant="fill-light"
-			color="primary"
-		>
-			Customization
-		</Button>
+<main>
+	<div class="p-2 grid place-items-start content-center">
+		<div class="text-left text-xl">Markov Bot Archive Page</div>
+		<div class="text-left text-lg">
+			Use the menu on the left to see the messages for each event.
+		</div>
 	</div>
 </main>
